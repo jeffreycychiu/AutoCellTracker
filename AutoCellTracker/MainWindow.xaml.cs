b51f865fedd01e52e4c04c39f0c3bfa3fb1751be
Jeff-MDLPC <j.chiu@alumni.ubc.ca>
2016-09-09 17:04:53 -0700

    added image navigation and flyout for settings

--- AutoCellTracker/MainWindow.xaml.cs
+++ AutoCellTracker/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
 
 namespace AutoCellTracker
 {
@@ -21,6 +24,11 @@ namespace AutoCellTracker
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        int numImages = 0;
+        int currentImage = 0;
+        List<String> imageFilePath = new List<string>();
+        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +36,69 @@ namespace AutoCellTracker
 
         private void selectFolder_Click(object sender, RoutedEventArgs e)
         {
+            //Select the folder which all the images are kept
+            //FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            //DialogResult result = folderBrowserDialog.ShowDialog();
 
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.Description = "Navigate to folder containing images";
+            dialog.UseDescriptionForTitle = true; // This applies to the Vista style dialog only, not the old dialog.
+            if (!VistaFolderBrowserDialog.IsVistaFolderDialogSupported)
+                MessageBox.Show(this, "Because you are not using Windows Vista or later, the regular folder browser dialog will be used. Please use Windows Vista to see the new dialog.", "Sample folder browser dialog");
+            if ((bool)dialog.ShowDialog(this))
+                folderTextBlock.Text = dialog.SelectedPath;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(dialog.SelectedPath);
+
+            //Searches folder for bitmap (.bmp) files. Loads an array with file names, and counts the total number of bmp files
+            numImages = 0;
+            currentImage = 0;
+
+            foreach (var imageFile in directoryInfo.GetFiles("*.bmp"))
+            {
+                imageFilePath.Add(imageFile.FullName);
+                numImages++;
+            }
+            updateImage();
+
+        }
+
+        public void updateImage()
+        {
+            ImageSource imageSource = new BitmapImage(new Uri(imageFilePath[currentImage]));
+            imageDisplay.Source = imageSource;
+            numImagesTextBlock.Text = "Image: " + (currentImage + 1) + "/" + numImages.ToString();
+        }
+
+        public void nextImage()
+        {
+            currentImage++;
+            if (currentImage > (numImages - 1))
+                currentImage = 0;
+            updateImage();
+        }
+
+        public void prevImage()
+        {
+            currentImage--;
+            if (currentImage < 0)
+                currentImage = numImages - 1;
+            updateImage();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            nextImage();
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            prevImage();
+        }
+
+        private void btnFlyoutSettings_Click(object sender, RoutedEventArgs e)
+        {
+            flyoutSettings.IsOpen = true;
         }
     }
 }
