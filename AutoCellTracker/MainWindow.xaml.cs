b167394accd012182dad5e3b1b7537083aa3840f
Jeff-MDLPC <j.chiu@alumni.ubc.ca>
2016-09-21 13:17:13 -0700

    added code from wpf sample to convert to bitmapimage source

--- AutoCellTracker/MainWindow.xaml.cs
+++ AutoCellTracker/MainWindow.xaml.cs
@@ -13,9 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
 using MahApps.Metro.Controls;
 using Microsoft.Win32;
 using Ookii.Dialogs.Wpf;
+using Emgu.CV;
 
 namespace AutoCellTracker
 {
@@ -105,6 +107,36 @@ namespace AutoCellTracker
         private void btnFlyoutSettings_Click(object sender, RoutedEventArgs e)
         {
             flyoutSettings.IsOpen = true;
+        }
+
+        /// <summary>
+        /// Delete a GDI object
+        /// </summary>
+        /// <param name="o">The poniter to the GDI object to be deleted</param>
+        /// <returns></returns>
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Convert an IImage to a WPF BitmapSource. The result can be used in the Set Property of Image.Source
+        /// </summary>
+        /// <param name="image">The Emgu CV Image</param>
+        /// <returns>The equivalent BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
         }
     }
 }
